849dfba560f4446c9563db582b269336cf71a9b4
CquptZouSheng <382223154@qq.com>
2016-09-29 23:02:44 +0800

    加在ViewPager。

--- app/src/main/java/com/zou/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/zou/android/criminalintent/CrimeActivity.java
@@ -3,9 +3,6 @@ package com.zou.android.criminalintent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 
 import java.util.UUID;
 
