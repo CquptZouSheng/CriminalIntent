ec7dc7a30b2e41548de5a88eafa746b6fbf0900d
CquptZouSheng <382223154@qq.com>
2016-09-30 11:10:52 +0800

    加载ViewPager。

--- app/src/main/java/com/zou/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/zou/android/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.zou.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -8,6 +10,7 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Zou on 2016/9/29.
@@ -17,11 +20,19 @@ public class CrimePagerActivity extends FragmentActivity {
 
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+    private static final String EXTRA_CRIME_ID ="crime_id";
 
+
+    public static Intent newIntent(Context context, UUID crimeId){
+        Intent intent=new Intent(context,CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID,crimeId);
+        return intent;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+        UUID crimeId= (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
         mViewPager= (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
         mCrimes=CrimeLab.get(this).getCrimes();
         FragmentManager fragmentManager=getSupportFragmentManager();
@@ -37,5 +48,11 @@ public class CrimePagerActivity extends FragmentActivity {
                 return mCrimes.size();
             }
         });
+        for (int i = 0; i <mCrimes.size() ; i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)){
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
     }
 }
