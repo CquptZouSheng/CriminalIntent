ffa0e6dfb1a53963d54713da48b0dab1b56c5f13
CquptZouSheng <382223154@qq.com>
2016-10-01 11:22:08 +0800

    添加工具栏。

--- app/src/main/java/com/zou/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/zou/android/criminalintent/SingleFragmentActivity.java
@@ -2,14 +2,14 @@ package com.zou.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Zou on 2016/9/27.
  */
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
     @Override
